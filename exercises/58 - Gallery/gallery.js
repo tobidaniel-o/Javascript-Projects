b8f53dce714dd23a0c405d34139383d1ba51f753
Oluwatobi Okunuga <tobi@macbook-pro.home>
2023-03-17 17:18:36 -0400

    added show image fnc

--- exercises/58 - Gallery/gallery.js
+++ exercises/58 - Gallery/gallery.js
@@ -6,13 +6,39 @@ function Gallery(gallery) {
     // select the elements we need
     const images = Array.from(gallery.querySelectorAll('img'))
     const modal = document.querySelector('.modal')
-    const prevButton = document.querySelector('.prev')
-    const nextButton = document.querySelector('.next')
+    const prevButton = modal.querySelector(".prev");
+	const nextButton = modal.querySelector(".next");
+	let currentImage;
+
+	function openModal() {
+		console.info("Opening Modal...");
+		// First check if the modal is already open
+		if (modal.matches(".open")) {
+			console.info("Modal already open");
+			return; //stop the function from running
+		}
+	}
+
+	function showImage(el) {
+		if (!el) {
+			console.log("no image to show");
+			return;
+		}
+		// update the modal with this info
+		console.log(el);
+		modal.querySelector("img").src = el.src;
+		modal.querySelector("h2").textContent = el.title;
+		modal.querySelector("figure p").textContent = el.dataset.description;
+		currentImage = el;
+		openModal();
+	}
+
+	images.forEach((image) =>
+		image.addEventListener("click", (e) => showImage(e.currentTarget))
+	);
 }
 
 // Use it on the page
+
 const gallery1 = Gallery(document.querySelector(".gallery1"));
 const gallery2 = Gallery(document.querySelector(".gallery2"));
-
-console.log(gallery1)
-console.log(gallery2)
